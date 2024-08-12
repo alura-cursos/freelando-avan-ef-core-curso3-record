fd6389904e301d5a6ab81cd9134837e9c6879e7b
bessax <andrebessax@gmail.com>
2024-08-12 10:46:04 -0300

    #Vídeo 1.2

--- src/Freelando.Api/Endpoints/PropostasExtensions.cs
+++ src/Freelando.Api/Endpoints/PropostasExtensions.cs
@@ -1,11 +1,24 @@
-﻿namespace Freelando.Api.Endpoints;
+﻿using Freelando.Dados.UnitOfWork;
+using Freelando.Modelos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace Freelando.Api.Endpoints;
 public static class PropostasExtensions
 {
     public static void AddEndPointPropostas(this WebApplication app)
     {
-        
+        app.MapGet("/propostas", async ([FromServices] IUnitOfWork unitOfOrk) =>
+        {
+            var propostas = await unitOfOrk.contexto.Propostas.FromSqlRaw("EXEC dbo.sp_BuscarTodasPropostas").ToListAsync();
 
-       
+           return Results.Ok(propostas);
+
+        }).WithTags("Propostas").WithOpenApi();
+
+
     }
 }
 
